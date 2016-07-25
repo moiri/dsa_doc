a7ac129b018bff658bd80d2f7806e7c5a497485f
Simon Maurer <moirelein@gmail.com>
2016-07-25 23:28:47 +0100

    change title of test page

--- web/index.php
+++ web/index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
 <head>
-<title>DSA Remote</title>
+<title>DSA Doc</title>
 <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 <meta name="DC.creator" content="Simon Maurer" />
 <meta name="DC.contributor" content="Ulisses Spiele" />
-<meta name="DC.title" content="DSA Remote" />
-<meta name="DC.date" content="2015-09-12" />
+<meta name="DC.title" content="DSA Doc" />
+<meta name="DC.date" content="2016-07-12" />
 <meta name="DC.language" content="en" />
 <link rel="stylesheet" type="text/css" href="plugin/bootstrap/css/bootstrap.min.css" />
 <link rel="stylesheet" type="text/css" href="plugin/bootstrap/css/bootstrap-theme.min.css" />
